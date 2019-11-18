efce0c3ca1453c43ac293baca3ef05b09258aa72
karim <karim@karims-MacBook-Pro.local>
2019-11-17 23:19:38 -0800

    add logging and chnage one letter vraiable names

--- TenantBase/tenantbase/interfaces/sql_interface.py
+++ TenantBase/tenantbase/interfaces/sql_interface.py
@@ -1,31 +1,38 @@
 import aiosqlite
 import asyncio
+import logging
 
+# GLOBALS
 DB = "tbase_cache.db"
+LOG = logging.getLogger()
+LOG.setLevel(logging.INFO)
 
 
-async def get_value_from_sql(k):
-    if k:
-        given_key = (k,)
+async def get_value_from_sql(key):
+    if key:
+        given_key = (key,)
         async with aiosqlite.connect(DB) as db:
             cursor = await db.execute("select value from kvalue where key=?", given_key)
             rows = await cursor.fetchall()
-            print(rows)
             return rows
+    else:
+        LOG.info(f"{key} not found in sqlite")
 
 
-async def delete_value_from_sql(k):
-    if k:
-        given_key = (k,)
+async def delete_value_from_sql(key):
+    if key:
+        given_key = (key,)
         async with aiosqlite.connect(DB) as db:
             cursor = await db.cursor()
             await cursor.execute("Delete from kvalue where key=?", given_key)
             await db.commit()
+    else:
+        LOG.info(f"{key} not found in sqlite")
 
 
-async def insert_value_into_sql(k, v):
-    if k and v:
-        given_key_value = (k, v)
+async def insert_value_into_sql(key, value):
+    if key and value:
+        given_key_value = (key, value)
 
         async with aiosqlite.connect(DB) as db:
             async with db.cursor() as cursor:
@@ -33,3 +40,5 @@ async def insert_value_into_sql(k, v):
                     "insert into kvalue( key , value) values (?,? )", given_key_value
                 )
                 await db.commit()
+    else:
+        LOG.info(f"{key}{value} was not provided by user")
